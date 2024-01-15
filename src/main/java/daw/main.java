eaea827030c242ad41de02b54a3503f9acb26a7e
ralcara908 <ralcara908@g.educaand.es>
2024-01-15 09:59:06 +0100

    creacion de objeto

---
+++ src/main/java/daw/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author rocio
+ */
+public class main {
+    public static void main(String[] args) {
+        Libreria  libro1= new Libreria(0234, "Miau");
+         Libreria  libro2= new Libreria(0734, "aaa");
+         System.out.println(libro1);
+         System.out.println(libro2);
+        
+        
+    }
+}
